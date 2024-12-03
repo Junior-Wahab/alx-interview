c49e7038160d49b8d181b3dcd51d7a9a5a7359b7
Junior-Wahab <jnrmwahab@gmail.com>
2024-12-03 12:29:37 +0000

    Task

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,37 +1,27 @@
 #!/usr/bin/python3
-"""
-Module for calculating the perimeter of an island described in a grid.
+"""Island Perimeter Problem
 """
 
 
 def island_perimeter(grid):
     """
-    Calculate the perimeter of the island in the grid.
-
+    Calculates the perimeter of the island described in grid
     Args:
-        grid (list of list of ints): The grid representation of the island.
-            0 represents water, 1 represents land.
-
-    Returns:
-        int: The perimeter of the island.
+        grid: 2d list of integers containing 0(water) or 1(land)
+    Return:
+        the perimeter of the island
     """
-    rows = len(grid)
-    cols = len(grid[0])
-    perimeter = 0
 
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:
-                # Add 4 for the land cell
-                perimeter += 4
-                # Subtract 1 for each adjacent land cell
-                if i > 0 and grid[i - 1][j] == 1:  # Check top
-                    perimeter -= 1
-                if i < rows - 1 and grid[i + 1][j] == 1:  # Check bottom
-                    perimeter -= 1
-                if j > 0 and grid[i][j - 1] == 1:  # Check left
-                    perimeter -= 1
-                if j < cols - 1 and grid[i][j + 1] == 1:  # Check right
-                    perimeter -= 1
-
-    return perimeter
+    p = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (grid[i][j] == 1):
+                if (i <= 0 or grid[i - 1][j] == 0):
+                    p += 1
+                if (i >= len(grid) - 1 or grid[i + 1][j] == 0):
+                    p += 1
+                if (j <= 0 or grid[i][j - 1] == 0):
+                    p += 1
+                if (j >= len(grid[i]) - 1 or grid[i][j + 1] == 0):
+                    p += 1
+    return p
