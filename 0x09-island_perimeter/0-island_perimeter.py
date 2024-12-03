ab436f3673c446680acdbc4071b303b5d67fc5ca
Junior-Wahab <jnrmwahab@gmail.com>
2024-12-03 12:42:28 +0000

    Task

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -11,21 +11,16 @@ def island_perimeter(grid):
     Return:
         the perimeter of the island
     """
-
     p = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                # Check top
                 if i <= 0 or grid[i - 1][j] == 0:
                     p += 1
-                # Check bottom
                 if i >= len(grid) - 1 or grid[i + 1][j] == 0:
                     p += 1
-                # Check left
                 if j <= 0 or grid[i][j - 1] == 0:
                     p += 1
-                # Check right
                 if j >= len(grid[i]) - 1 or grid[i][j + 1] == 0:
                     p += 1
     return p
